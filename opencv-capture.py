1f94e92b6a9905a036844f0f6436472c4fac7326
weaq <oooweaqooo@gmail.com>
2020-09-22 10:48:31 +0700

    Initial commit

---
+++ opencv-capture.py
@@ -0,0 +1,28 @@
+import numpy as np
+import cv2, time
+
+# Read video from source
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame by frame
+    ret, frame = cap.read()
+
+    # Wait for keyboard press
+    k = cv2.waitKey(1)
+    # Press q to exit
+    if k == ord('q'):
+        break
+    # Press s to save
+    if k == ord('s'):
+        timestr = time.strftime("%Y%m%d%H%M%S")
+        # Write capture to file
+        cv2.imwrite('img/' + timestr + '.jpg',frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Show image
+    cv2.imshow('frame',frame)
+
+# Release capture when done
+cap.release()
+cv2.destroyAllWindows()
